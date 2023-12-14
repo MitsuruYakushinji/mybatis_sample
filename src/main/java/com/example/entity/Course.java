a408cbf136a25db4a52d2a81f3947599b9b008f2
MitsuruYakushinji <141882459+MitsuruYakushinji@users.noreply.github.com>
2023-12-14 16:20:27 +0900

    完了

--- src/main/java/com/example/entity/Course.java
+++ src/main/java/com/example/entity/Course.java
@@ -1,9 +1,12 @@
 package com.example.entity;
 
+import java.util.List;
+
 public class Course {
 	private int id;
 	private String name;
 	private CourseDetail courseDetail;
+	private List<Chapter> chapters;
 	
 	public int getId() {
         return id;
@@ -24,5 +27,12 @@ public class Course {
     }
     public void setCourseDetail(CourseDetail courseDetail) {
         this.courseDetail = courseDetail;
+    }
+    
+    public List<Chapter> getChapters(){
+    	return chapters;
+    }
+    public void setChapters(List<Chapter> chapters) {
+    	this.chapters = chapters;
     }
 }
